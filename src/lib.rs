f9cec58983f3024febeb9deaf65f964ac1df027f
Cody P Schafer <dev@codyps.com>
2015-02-18 19:47:00 -0500

    initial

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+#[macro_export]
+macro_rules! some_error {
+    ($call:expr) => (
+        match $call {
+            Some(e) => panic!("{} => {:?}", stringify!($call), e),
+            None => ()
+        }
+    )
+}
+
+#[macro_export]
+macro_rules! or_panic {
+    ($e:expr) => ({
+        let v = $e;
+        let s = stringify!($e);
+        or_panic!(v, "failure in {:?} = {:?}", s, v.err())
+    });
+    ($e:expr, $fmt:expr, $($arg:tt)+) => (
+        match $e {
+            Ok(e) => e,
+            Err(_) => {
+                panic!($fmt, $($arg)+)
+            }
+        }
+    );
+}
+
+#[macro_export]
+macro_rules! from_error  {
+    ($enum_base:ident => $enum_elem:ident($elem_type:ty)) => (
+        impl ::std::error::FromError<$elem_type> for $enum_base {
+            fn from_error(err: $elem_type) -> $enum_base {
+                $enum_base::$enum_elem(err)
+            }
+        }
+    )
+}
+
+
+#[cfg(test)]
+mod test {
+
+    #[derive(Debug)]
+    pub enum AnError {
+        Foo(&'static str)
+    }
+
+    from_error! { AnError => Foo(&'static str) }
+
+    fn f() -> Result<(),AnError> {
+        try!(Err("hi"))
+    }
+
+    #[test]
+    fn it_works() {
+        or_panic!( {
+            let x : Result<(), &'static str> = Ok(());
+            x;
+        });
+
+        println!("Got: {:?}", f())
+    }
+
+
+
+}
